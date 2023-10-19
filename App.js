9fda9ae3c43c538307906fefd3fdedad9410ba42
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-10-18 23:29:06 -0300

    veremos...

--- App.js
+++ App.js
@@ -13,7 +13,10 @@ import {
   Box,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Platform } from "react-native";
+import Login from "./views/Login";
 
 // Define the config
 const config = {
@@ -22,52 +25,28 @@ const config = {
 };
 
 // extend the theme
-export const theme = extendTheme({ config });
+//export const theme = extendTheme({ config });
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+
+
+<NavigationContainer>
+  <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Login" component={Login} />
+  </Stack.Navigator>
+</NavigationContainer>
+
+
+      
     </NativeBaseProvider>
   );
 }
 
 // Color Switch Component
-function ToggleDarkMode() {
+/* function ToggleDarkMode() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack space={2} alignItems="center">
@@ -82,4 +61,4 @@ function ToggleDarkMode() {
       <Text>Light</Text>
     </HStack>
   );
-}
+} */
